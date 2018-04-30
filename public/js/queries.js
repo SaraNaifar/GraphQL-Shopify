25409834344176680a96104318ca9aec6606291e
sara <naifarsara@gmail.com>
2018-04-30 22:00:33 +0100

    final commit

--- public/js/queries.js
+++ public/js/queries.js
@@ -1,45 +1,81 @@
-var storeFrontAPI = "https://name-of-your-store.myshopify.com/api/graphql";
-var storeFrontAccessToken = "";
+var storeFrontAPI = 'https://geekmind-store.myshopify.com/api/graphql'
+var storeFrontAccessToken = 'd0eb36c7715140c8c9ba840a5955009b'
 
 // Sets up the GraphQL request headers
-function makeRequest(query) {
+function makeRequest (query) {
   var headers = {
-    "X-Shopify-Storefront-Access-Token": storeFrontAccessToken,
-    "Content-Type": "application/json"
-  };
+    'X-Shopify-Storefront-Access-Token': storeFrontAccessToken,
+    'Content-Type': 'application/json'
+  }
 
   return $.ajax({
     url: storeFrontAPI,
-    type: "POST",
+    type: 'POST',
     data: JSON.stringify({ query: query }),
     headers: headers
-  });
+  })
 }
 
 // Queries for product information
-function fetchProducts() {
+function fetchProducts () {
   var query = `
+  {
+  shop {
+    products(first:4) {
+      edges {
+        node {
+          title,
+          images (first: 1){
+            edges{
+              node{
+                src
+              }
+            }
+          },
+         variants(first: 1){
+          edges{
+            node {
+              id
+            }
+          }
+        }
+        }
+      }
+    }
+  }
+}
 
-  `;
 
-  return makeRequest(query);
+  `
+
+  return makeRequest(query)
 }
 
 // // Buys new power up by creating new checkout with item
-// function buyPowerUp(variantId) {
-//   var query = `
-//     mutation {
-//       checkoutCreate() {
-//
-//         }
-//       }
-//     `;
-//
-//   return makeRequest(query);
-// }
-//
+function buyPowerUp (variantId) {
+  var query = `
+     mutation {
+     checkoutCreate(input:{
+      lineItems: [{
+        variantId: "${variantId}",
+        quantity:1
+      }]
+     }) {
+      checkout{
+        webUrl,
+        completedAt,
+        id
+      }
+
+          }
+        }
+     `
+
+  return makeRequest(query)
+}
+
 // Checks completed purchases by querying checkouts with `completedAt` value
-function checkCompletedPurchases(checkoutIds) {
+function checkCompletedPurchases (checkoutIds) {
   var query = `
       query {
         nodes(ids: [${checkoutIds}]) {
@@ -61,7 +97,7 @@ function checkCompletedPurchases(checkoutIds) {
           }
         }
       }
-    `;
+    `
 
-  return makeRequest(query);
+  return makeRequest(query)
 }
